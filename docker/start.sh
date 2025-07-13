55ee149f172809a412194ebb240789eea40ebdf0
root <root@telerising-mirror>
2025-07-13 16:52:16 +0200

    change docker to wget

--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 apk add --no-cache bash
-curl -s https://raw.githubusercontent.com/DEvmIb/telerising-helper/refs/heads/main/api.sh|bash -s -- /telerising
+wget -o - https://raw.githubusercontent.com/DEvmIb/telerising-helper/refs/heads/main/api.sh|bash -s -- /telerising
