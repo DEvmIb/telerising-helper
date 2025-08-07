b986dd0a2ec69567582d6ef707f7dcaefc8645b6
root <root@debian-armv7.bunker.home>
2025-08-07 10:30:21 -0400

    hf

--- packages/appimage/build_base_armhf_raspbian.sh
+++ packages/appimage/build_base_armhf_raspbian.sh
@@ -3,9 +3,7 @@ then
 	apt -y install file wget fuse3
 fi
 
-_app_image_build=https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-armhf.AppImage 
-_app_image_build=https://github.com/AppImage/appimagetool/releases/download/1.9.0/appimagetool-armhf.AppImage
-_app_image_build=https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-armhf.AppImage
+_app_image_build=https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-armhf.AppImage
 wget -q "$_app_image_build" -O appimagetool.AppImage
 chmod +x appimagetool.AppImage
 ./appimagetool.AppImage skelet_armhf_raspbian armhf_raspbian.AppImage
